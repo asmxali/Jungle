25c460aa9ddd82271941c7f45e02090df94a3adb
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-04-07 20:17:36 +0000

    added validation and class method .authenticate_with_credentials for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   def new
   end
 
+
+
   def create
+    
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -11,6 +14,8 @@ class UsersController < ApplicationController
     else
       redirect_to '/signup'
     end
+
+    
   end
 
   private 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email
+  validates :password, presence: true, length: { minimum: 8}
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email,password)
+    # If the user exists AND the password entered is correct.
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    end 
+  end
+
 end
